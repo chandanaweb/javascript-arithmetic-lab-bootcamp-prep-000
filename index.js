7da38a20839cc85bb6c432cc72864dffda3b7a96
chandu11288@gmail.com <chandu11288@gmail.com>
2017-12-28 04:35:51 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,6 +10,15 @@ function multiply(a,b){
 function divide(a,b){
   return a/b;
 };
-function inc(a){return ++a ;};
- function dec(a){return --a ;};
-function makeInt(n){return parseInt(n,10)};
+function inc(a){
+  return ++a ;
+};
+ function dec(a){
+   return --a ;
+ };
+function makeInt(n){
+  return parseInt(n,10)
+};
+function preserveDecimal(n){
+  return parseFloat(n);
+};
